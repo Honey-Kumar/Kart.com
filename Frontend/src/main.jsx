fc74f489c23585a41118b4674282987c8a6bb258
HONEY KUMAR <honeykumar987636@gmail.com>
2024-10-02 13:57:25 +0530

    second commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from './Redux/store.jsx'
+import { persistor, store } from './Redux/Store.jsx'
 
 
 createRoot(document.getElementById('root')).render(
